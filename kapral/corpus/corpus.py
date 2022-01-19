44e00be43161453af861fa607d55724eca08cbb1
Sasha <alexander.drozd@gmail.com>
2022-01-19 10:04:35 +0900

    add corpus submodule

---
+++ kapral/corpus/corpus.py
@@ -0,0 +1,245 @@
+import logging
+import os
+from collections import namedtuple
+
+import numpy as np
+from vecto.utils.data import get_uncompressed_size
+from vecto.utils.metadata import WithMetaData
+
+from .iterators import (CharIterator, DirIterator, FileIterator,
+                        FileLineIterator, LoopedLineIterator, SequenceIterator,
+                        SlidingWindowIterator, TokenIterator,
+                        TokenizedSequenceIterator, ViewLineIterator)
+from .tokenization import (DEFAULT_JAP_TOKENIZER, DEFAULT_SENT_TOKENIZER,
+                           DEFAULT_TOKENIZER)
+
+logger = logging.getLogger(__name__)
+
+
+TreeElement = namedtuple('TreeElement', ["filename", "bytes"])
+
+
+class BaseCorpus(WithMetaData):
+    """Cepresents a body of text in single or multiple files"""
+
+    def __init__(self, path, language='eng'):
+        super().__init__(path)
+        self.path = path
+        self.language = language
+
+    def get_sliding_window_iterator(self,
+                                    left_ctx_size=2,
+                                    right_ctx_size=2,
+                                    tokenizer=None,
+                                    verbose=0):
+        if tokenizer is None:
+            if self.language == 'jap':
+                tokenizer = DEFAULT_JAP_TOKENIZER
+            else:
+                tokenizer = DEFAULT_TOKENIZER
+        return SlidingWindowIterator(
+            self.get_sentence_iterator(tokenizer=tokenizer),
+            left_ctx_size=left_ctx_size,
+            right_ctx_size=right_ctx_size)
+
+    def get_token_iterator(self, tokenizer=None, verbose=False):
+        if tokenizer is None:
+            if self.language == 'jap':
+                tokenizer = DEFAULT_JAP_TOKENIZER
+            else:
+                tokenizer = DEFAULT_TOKENIZER
+        return TokenIterator(self.get_sentence_iterator(tokenizer, verbose))
+
+    def get_character_iterator(self, verbose=False):
+        return CharIterator(self.get_line_iterator(verbose))
+
+    def get_sentence_iterator(self, tokenizer=None, verbose=False):
+        if tokenizer is None:
+            if self.language == 'jap':
+                tokenizer = DEFAULT_JAP_TOKENIZER
+            else:
+                tokenizer = DEFAULT_SENT_TOKENIZER
+        return TokenizedSequenceIterator(self.get_line_iterator(verbose=verbose),
+                                         tokenizer=tokenizer,
+                                         verbose=verbose)
+
+    def get_sequence_iterator(self, sequence_length, tokenizer):
+        return SequenceIterator(self.get_line_iterator(),
+                                sequence_length=sequence_length,
+                                tokenizer=tokenizer)
+
+    def get_looped_sequence_iterator(self, sequence_length, tokenizer, rank, size, min_length=0, reset_on_new_line=False):
+        return SequenceIterator(self.get_looped_line_iterator(rank, size),
+                                sequence_length=sequence_length,
+                                tokenizer=tokenizer,
+                                minimal_length=min_length,
+                                reset_on_new_line=reset_on_new_line)
+
+
+class Corpus(BaseCorpus):
+    def load_dir_strucute(self):
+        self.tree = []
+        accumulated_size = 0
+        for file in DirIterator(self.path):
+            accumulated_size += get_uncompressed_size(file)
+            self.tree.append(TreeElement(file, accumulated_size))
+        self.metadata["total_bytes"] = self.total_bytes
+        # print(self.tree)
+        # TODO: use named tuples here
+        # self.tree = [TreeElement("file1", 10), TreeElement("file2", 15)]
+
+    @property
+    def total_bytes(self):
+        return self.tree[-1].bytes
+
+    def get_file_and_offset(self, global_position, start_of_range=True, epsilon=0):
+        assert global_position <= self.total_bytes
+        lo = 0
+        hi = len(self.tree)
+        while (True):
+            current = (lo + hi) // 2
+            # print(f"lo {lo}, hi {hi}, pos {pos}")
+            if lo >= hi:
+                if current > 0:
+                    offset = max(global_position - self.tree[current - 1].bytes, 0)
+                else:
+                    offset = global_position
+                if start_of_range:
+                    if self.tree[current].bytes - global_position < epsilon:
+                        if current < len(self.tree) - 1:
+                            current += 1
+                            offset = 0
+                else:
+                    if current > 0:
+                        if offset < epsilon:
+                            offset = self.tree[current - 1].bytes - (self.tree[current - 2].bytes if current > 1 else 0)
+                            current -= 1
+                return current, offset
+
+            if self.tree[current].bytes >= global_position:
+                hi = current
+            if self.tree[current].bytes < global_position:
+                lo = current + 1
+
+    def get_line_iterator(self, verbose=False):
+        # TODO: can be more optimal w/o using view
+        return CorpusView(self, 0, 1).get_line_iterator()
+
+    def get_looped_line_iterator(self, rank=0, size=1):
+        assert rank < size
+        byte_start = self.total_bytes * rank // size
+        node_start = self.get_file_and_offset(byte_start, start_of_range=True, epsilon=0)
+        iterator = LoopedLineIterator(self.tree, node_start)
+        return iterator
+
+
+class CorpusView(BaseCorpus):
+    def __init__(self, file_corpus, rank, size):
+        assert rank < size
+        self.corpus = file_corpus
+        self.rank = rank
+        self.size = size
+
+    def get_line_iterator(self, verbose=False):
+        byte_start, byte_end = self.rank_and_size_to_pos(self.rank, self.size)
+        # TODO: read epsilon from config ^_^
+        node_start = self.corpus.get_file_and_offset(byte_start, start_of_range=True, epsilon=0)
+        node_end = self.corpus.get_file_and_offset(byte_end, start_of_range=False, epsilon=0)
+        # CREATE ITERATOR HERE
+        # iterate over precomputed tree of files and sizes
+        # iterated this file/this offset to last-file last offset
+        iterator = ViewLineIterator(self.corpus.tree, verbose=False, start=node_start, end=node_end)
+        return iterator
+
+    def rank_and_size_to_pos(self, rank, size):
+        assert rank < size
+        start = self.corpus.total_bytes * rank // size
+        end = self.corpus.total_bytes * (rank + 1) // size
+        return start, end
+
+
+# TODO: make this deprecated and use Corpus instead
+class FileCorpus(BaseCorpus):
+    """Cepresents a body of text in a single file"""
+
+    def get_line_iterator(self, verbose=False):
+        return FileLineIterator(FileIterator(self.path, verbose=verbose))
+
+
+class DirCorpus(BaseCorpus):
+    """Cepresents a body of text in a directory"""
+
+    def get_line_iterator(self, verbose=False):
+        return FileLineIterator(DirIterator(self.path, verbose=verbose))
+
+
+# old code below ----------------------------------
+
+
+# def FileSlidingWindowCorpus(path, left_ctx_size=2, right_ctx_size=2, tokenizer=DEFAULT_TOKENIZER, verbose=0):
+#    """
+#    Reads text from `path` line-by-line, splits each line into tokens and/or sentences (depending on tokenizer),
+#    and yields training samples for prediction-based distributional semantic models (like Word2Vec etc).
+#    Example of one yielded value: {'current': 'long', 'context': ['family', 'dashwood', 'settled', 'sussex']}
+#    :param path: text file to read (can be archived)
+#    :param tokenizer: tokenizer to use to split into sentences and tokens
+#    :param verbose: whether to enable progressbar or not
+#    :return:
+#    """
+#    return SlidingWindowIterator(
+#        TokenizedSequenceIterator(
+#            FileLineIterator(
+#                FileIterator(path, verbose=verbose)),
+#            tokenizer=tokenizer),
+#        left_ctx_size=left_ctx_size,
+#        right_ctx_size=right_ctx_size)
+
+
+def DirSlidingWindowCorpus(path, left_ctx_size=2, right_ctx_size=2, tokenizer=DEFAULT_TOKENIZER, verbose=0):
+    """
+    Reads text from all files from all subfolders of `path` line-by-line,
+    splits each line into tokens and/or sentences (depending on `tokenizer`),
+    and yields training samples for prediction-based distributional semantic models (like Word2Vec etc).
+    Example of one yielded value: {'current': 'long', 'context': ['family', 'dashwood', 'settled', 'sussex']}
+    :param path: text file to read (can be archived)
+    :param tokenizer: tokenizer to use to split into sentences and tokens
+    :param verbose: whether to enable progressbar or not
+    :return:
+    """
+    return SlidingWindowIterator(
+        TokenizedSequenceIterator(
+            FileLineIterator(
+                DirIterator(path, verbose=verbose)),
+            tokenizer=tokenizer),
+        left_ctx_size=left_ctx_size,
+        right_ctx_size=right_ctx_size)
+
+
+def corpus_chain(*corpuses):
+    """
+    Join all copuses into a big single one. Like `itertools.chain`, but with proper metadata handling.
+    :param corpuses: other corpuses or iterators
+    :return:
+    """
+    return IteratorChain(corpuses)
+
+
+# TODO: make it a part of Corpus class
+def load_path_as_ids(path, vocabulary, tokenizer=DEFAULT_TOKENIZER):
+    # use proper tokenizer from cooc
+    # options to ignore sentence bounbdaries
+    # specify what to do with missing words
+    # replace numbers with special tokens
+    result = []
+    if os.path.isfile(path):
+        # TODO: why file corpus does not need language? 
+        ti = FileCorpus(path).get_token_iterator(tokenizer=tokenizer)
+    else:
+        if os.path.isdir(path):
+            ti = DirCorpus(path).get_token_iterator(tokenizer)
+        else:
+            raise RuntimeError("source file does not exist")
+    for token in ti:
+        w = token  # specify what to do with missing words
+        result.append(vocabulary.get_id(w))
+    return np.array(result, dtype=np.int32)
---
+++ kapral/corpus/iterators.py
@@ -0,0 +1,305 @@
+import collections
+import fnmatch
+import logging
+import os
+
+from vecto.corpus.base import BaseIterator
+from vecto.corpus.tokenization import DEFAULT_SENT_TOKENIZER, DEFAULT_TOKENIZER
+from vecto.utils.data import detect_archive_format_and_open
+
+logger = logging.getLogger(__name__)
+
+
+class FileIterator(BaseIterator):
+    """
+    Iterator which yields only given filename.
+    """
+
+    def __init__(self, filename, verbose=0):
+        super(FileIterator, self).__init__(base_path=filename,
+                                           verbose=verbose)
+        self.filename = filename
+
+    def _generate_samples(self):
+        yield self.filename
+
+
+class DirIterator(BaseIterator):
+    """
+    Iterator which yield all files in the given folder and all its subfolders.
+    """
+
+    def __init__(self, dirname, verbose=0):
+        super(DirIterator, self).__init__(base_path=dirname,
+                                          verbose=verbose)
+        self.dirname = dirname
+
+    def _generate_samples(self):
+        if os.path.isfile(self.dirname):
+            yield self.dirname
+        else:
+            for root, _, files in os.walk(self.dirname, followlinks=True):
+                for good_fname in sorted(fnmatch.filter(files, "*")):
+                    full_file_path = os.path.join(root, good_fname)
+                    logger.info("processing " + full_file_path)
+                    yield full_file_path
+
+
+class FileLineIterator(BaseIterator):
+    """
+    Receives a sequence of filenames from `base_corpus` and reads each file line-by-line.
+    """
+
+    def __init__(self, base_corpus, verbose=0):
+        super(FileLineIterator, self).__init__(base_corpus=base_corpus.metadata,
+                                               verbose=verbose)
+        self.base_corpus = base_corpus
+
+    def _generate_samples(self):
+        for filename in self.base_corpus:
+            with detect_archive_format_and_open(filename) as file_in:
+                for line in file_in:
+                    line = line.strip()
+                    if line:
+                        yield line
+
+
+def seek_unicode(fp, position, direction=-1):
+    while position >= 0:
+        fp.seek(position)
+        try:
+            fp.seek(position)
+            fp.read(1)
+            fp.seek(position)
+            return
+        except UnicodeDecodeError:
+            position += direction
+    raise UnicodeDecodeError("File not decodable")
+
+
+class ViewLineIterator(BaseIterator):
+    def __init__(self, tree, start, end, verbose):
+        # TODO: sort this stuff from parent class out
+        super().__init__(base_corpus=None, verbose=verbose)
+        self.tree = tree
+        self.start = start
+        self.end = end
+
+    def _generate_samples(self):
+        for i in range(self.start[0], self.end[0] + 1):
+            filename = self.tree[i].filename
+            with detect_archive_format_and_open(filename) as file_in:
+                if i == self.start[0]:
+                    # TODO: conside seek to beginning of line
+                    seek_unicode(file_in, self.start[1])
+                cnt_bytes_read = self.start[0] if self.start[0] == self.end[0] else 0
+                for line in file_in:
+                    cnt_bytes_read += len(line)
+                    line = line.strip()
+                    if cnt_bytes_read > self.end[1]:
+                        break
+                    yield line
+
+
+class LoopedLineIterator(BaseIterator):
+    def __init__(self, tree, start):
+        super().__init__()
+        self.tree = tree
+        self.id_file = start[0]
+        self.start_offset = start[1]
+        self._cnt_restarts = 0
+
+    def _generate_samples(self):
+        filename = self.tree[self.id_file][0]
+        file_in = detect_archive_format_and_open(filename)
+        seek_unicode(file_in, self.start_offset)
+        file_in.readline()
+        while True:
+            for line in file_in:
+                line = line.strip()
+                yield line
+            file_in.close()
+            self.id_file += 1
+            if self.id_file >= len(self.tree):
+                self.id_file = 0
+                self._cnt_restarts += 1
+            file_in = detect_archive_format_and_open(self.tree[self.id_file][0])
+
+    @property
+    def cnt_restarts(self):
+        return self._cnt_restarts
+
+
+class TokenizedSequenceIterator(BaseIterator):
+    """
+    Receives any corpus yielding text (e.g. `FileLineIterator`) and produces tokenized sequences.
+    Good for splitting texts on sentences.
+    """
+
+    def __init__(self, base_corpus, tokenizer=DEFAULT_TOKENIZER, verbose=0):
+        super(TokenizedSequenceIterator, self).__init__(base_corpus=base_corpus.metadata,
+                                                        tokenizer=tokenizer.metadata,
+                                                        verbose=verbose)
+        self.base_corpus = base_corpus
+        self.tokenizer = tokenizer
+
+    def _generate_samples(self):
+        for line in self.base_corpus:
+            # TODO: sentence may span over multiple lines, we should take this into account somehow
+            # I think that it's better to ignore this here and write docs like:
+            # "You should be aware of that and prepare your data accordingly, e.g. one line - one real doc"
+            tokenized = self.tokenizer(line.strip())
+            for tokenized_sentence in tokenized:
+                yield tokenized_sentence
+
+
+class SequenceIterator(BaseIterator):
+    def __init__(self, line_terator, sequence_length, tokenizer, minimal_length=0, reset_on_new_line=False):
+        super().__init__()
+        self.line_iterator = line_terator
+        self.sequence_length = sequence_length
+        self.tokenizer = tokenizer
+        self.buffer = []
+        self.minimal_length = minimal_length
+        self.reset_on_new_line = reset_on_new_line
+
+    def _generate_samples(self):
+        # TODO: consider removing too small chunks of sentences at the end
+        # TODO: consider leveraging sentence iterator is corpus has mark-up
+        for line in self.line_iterator:
+            tokens = self.tokenizer(line)
+            if self.reset_on_new_line:
+                self.buffer = []
+            elif len(self.buffer) < self.minimal_length:
+                self.buffer = []
+            self.buffer += tokens
+            while len(self.buffer) > self.sequence_length - self.minimal_length:
+                s = self.buffer[: self.sequence_length]
+                self.buffer = self.buffer[self.sequence_length:]
+                yield s
+
+    @property
+    def cnt_restarts(self):
+        # TODO: this will fail with non-looped line iterator,
+        # maybe there's a way to do it more gracefully
+        return self.line_iterator.cnt_restarts
+
+
+class BaseNestedIterator(BaseIterator):
+    def __init__(self, parent_iterator, verbose=0):
+        # TODO: this .metadata seems strange
+        super().__init__(parent_iterator=parent_iterator.metadata,
+                         verbose=verbose)
+        self.parent_iterator = parent_iterator
+
+
+class TokenIterator(BaseNestedIterator):
+    def _generate_samples(self):
+        for tokenized_str in self.parent_iterator:
+            for token in tokenized_str:
+                yield token
+
+
+class CharIterator(BaseNestedIterator):
+    def _generate_samples(self):
+        for line in self.parent_iterator:
+            for c in line:
+                yield c
+            yield " "
+
+
+def iter_sliding_window(seq, left_ctx_size, right_ctx_size):
+    for i, current in enumerate(seq):
+        ctx = []
+        ctx.extend(seq[i - left_ctx_size: i])
+        ctx.extend(seq[i + 1: i + right_ctx_size + 1])
+        yield i, current, ctx
+
+
+class SlidingWindowIterator(BaseIterator):
+    """
+    Receives any corpus yielding sequences of tokens (e.g. TokenizedSequenceIterator)
+    and produces training samples for prediction-based distributional semantic models (like Word2Vec etc).
+    Example of one yielded value: {'current': 'long', 'context': ['family', 'dashwood', 'settled', 'sussex']}
+    """
+
+    def __init__(self, base_corpus, left_ctx_size=2, right_ctx_size=2, verbose=0):
+        assert isinstance(next(iter(base_corpus)), collections.abc.Sequence)
+        super(SlidingWindowIterator, self).__init__(base_corpus=base_corpus.metadata,
+                                                    left_ctx_size=left_ctx_size,
+                                                    right_ctx_size=right_ctx_size,
+                                                    verbose=verbose)
+        self.base_corpus = base_corpus
+        self.left_ctx_size = left_ctx_size
+        self.right_ctx_size = right_ctx_size
+        self.__gen__  = self._generate_samples()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return next(self.__gen__)
+
+    def _generate_samples(self):
+        for sample_elems in self.base_corpus:
+            for _, current, ctx in iter_sliding_window(sample_elems,
+                                                       self.left_ctx_size,
+                                                       self.right_ctx_size):
+                yield dict(current=current,
+                           context=ctx)
+
+
+# class SlidingWindowAndGlobal(BaseIterator):
+#     def __init__(self, base_corpus, left_ctx_size=2, right_ctx_size=2, verbose=0):
+#         assert isinstance(next(iter(base_corpus)), collections.abc.Sequence)
+#         super(SlidingWindowAndGlobal, self).__init__(base_corpus=base_corpus.metadata,
+#                                                      left_ctx_size=left_ctx_size,
+#                                                      right_ctx_size=right_ctx_size,
+#                                                      verbose=verbose)
+#         self.base_corpus = base_corpus
+#         self.left_ctx_size = left_ctx_size
+#         self.right_ctx_size = right_ctx_size
+
+#     def _generate_samples(self):
+#         for sample_elems in self.base_corpus:
+#             for _, current, ctx in iter_sliding_window(sample_elems,
+#                                                        self.left_ctx_size,
+#                                                        self.right_ctx_size):
+#                 yield dict(current=current,
+#                            context=ctx,
+#                            global_context=list(sample_elems))
+
+
+# class IteratorChain(BaseIterator):
+#    """
+#    Like `itertools.chain`, but with proper metadata handling
+#    """
+#    def __init__(self, base_iterators, verbose=0):
+#        super(IteratorChain, self).__init__(base_iterators=[i.metadata for i in base_iterators],
+#                                            verbose=verbose)
+#        self.base_iterators = base_iterators
+
+#    def _generate_samples(self):
+#        for base_iter in self.base_iterators:
+#            for sample in base_iter:
+#                yield sample
+
+
+# class TruncatedCorpus(BaseIterator):
+#    """
+#    Reads first `limit` samples from `base_corpus` and yields them sample-by-sample.
+#    Good for debugging.
+#    """
+#    def __init__(self, base_corpus, limit=1000, verbose=0):
+#        super(TruncatedCorpus, self).__init__(base_corpus=base_corpus.meta,
+#                                              verbose=verbose)
+#        self.samples = []
+#        for i, s in enumerate(base_corpus):
+#            if i >= limit:
+#                break
+#            self.samples.append(s)
+#        self.metadata['samples_count'] = len(self.samples)
+
+#    def _generate_samples(self):
+#        for s in self.samples:
+#            yield s
