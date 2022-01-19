6c7e4a2780712e1fc43f685035526e51d18aa625
Sasha <alexander.drozd@gmail.com>
2022-01-19 09:55:10 +0900

    initial

---
+++ kapral/__init__.py
@@ -0,0 +1,15 @@
+"""kapral is a library for all things related to corpus linguistics
+
+Submodules
+==========
+
+.. autosummary::
+    :toctree: _autosummary
+    corpus
+    vocabulary
+"""
+
+from ._version import VERSION
+
+
+__version__ = VERSION
---
+++ kapral/_version.py
@@ -0,0 +1,3 @@
+"""Version of kapral package."""
+
+VERSION = "0.0.1"
